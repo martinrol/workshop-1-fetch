54071547892c404e59a48ff093c5897348caee98
Martin Roldan <martinroldan889@gmail.com>
2021-01-22 09:17:04 -0300

    Consultando a una API externa y creando nodos para luego insertar información de la misma

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+const fetchData = async () => {
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    try {
+        const allOfItems = [];
+
+        responseJson.data.forEach((item) => {
+        const image = document.createElement('img');
+        const title = document.createElement('h2');
+        const price = document.createElement('p');
+
+        const container = document.createElement('div');
+        container.append(image, title, price);
+
+        allOfItems.push(container)
+    });
+
+    document.body.append(...allOfItems);
+    } catch(error) {
+        console.error(error.message);
+    }
+}
+fetchData()
