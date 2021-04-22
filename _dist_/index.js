7031deb94b3d6c45bf18755f633f7443de61ea6c
mxrold <mxrold@users.noreply.github.com>
2021-04-22 00:13:01 +0000

    deploy: 136e8f2d89d56806512b57c5cd877cdfccb25e1d

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,6 +2,12 @@ const baseUrl = "https://platzi-avo.vercel.app";
 
 const appNode = document.querySelector('#app');
 
+appNode.addEventListener("click", (event) => {
+    if(event.target.nodeName === 'H2') {
+        window.alert('Hola')
+    }
+})
+
 // Formato a monedas
 const formatPrice = (price) => {
     const newPrice = new window.Intl.NumberFormat('en-EN', {
